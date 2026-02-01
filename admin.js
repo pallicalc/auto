6231b6b73b3b3c7b86c1b568de9fd299e8ef5e9a
pallicalc <pallicalc.gmail.com>
2026-02-01 09:40:10 +0800

    favicon

--- admin.js
+++ admin.js
@@ -1,4 +1,17 @@
-﻿// admin.js - CLEANED VERSION
+﻿// --- Auto-Inject Favicon into Header ---
+(function() {
+    let link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
+    }
+    // Adjust path based on where this script runs. 
+    // If diagnostic.js is in a subfolder, use '../favicon.png'
+    link.href = '/favicon.png'; 
+})();
+
+// admin.js - CLEANED VERSION
 
 // NOTE: Firebase is already initialized in Admin.html
 // We just grab the existing instances here.
